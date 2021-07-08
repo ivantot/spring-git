a4a0c9148277af5233668fe5b70274314d4061cf
Ivan Tot <ivan.tot@gmail.com>
2021-07-08 21:08:11 +0200

    add first commit

---
+++ src/test/java/Brains2021/git/SpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package Brains2021.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
